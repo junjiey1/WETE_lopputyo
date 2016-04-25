b566a5eeb21b9f3b274e7357e04b806ff61f4cce
wete_Kasperi_Närhi <kasperi.n1995@gmail.com>
2016-04-25 06:54:20 +0000

    Pohja

---
+++ js/map.js
@@ -0,0 +1,38 @@
+"use strict";
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 60.192059, lng: 24.945831},
+    zoom: 12,
+    styles: [{
+      featureType: 'poi',
+      stylers: [{ visibility: 'off' }]  // Turn off points of interest.
+    }, {
+      featureType: 'transit.station',
+      stylers: [{ visibility: 'on' }]  // Turn off bus stations, train stations, etc.
+    }],
+    disableDoubleClickZoom: true
+  });
+  
+}
+
+
+function getRoute(){
+    var url="php/getloc.php";
+    var pyynto=new XMLHttpRequest();
+    var sijainnit;
+    
+    pyynto.onreadystatechange=function(){
+        if (pyynto.readyState ==4 && pyynto.status==200){
+            sijainnit=pyynto.responseText;
+            console.log(sijainnit);
+        }
+        
+        
+    }
+    
+    pyynto.open('get', url,true);
+    pyynto.send();
+}
+
+getRoute();
