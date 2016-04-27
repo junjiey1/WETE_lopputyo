ea5450c4da4c5fdb7c12604b433734ba6546c8a5
wete_Kasperi_Närhi <kasperi.n1995@gmail.com>
2016-04-27 16:15:11 +0000

    Sijainninpaivitus tehty tietokantaan updater.php

--- php/getloc.php
+++ php/getloc.php
@@ -1,42 +1,60 @@
    <?php
+   
+    //Tämä on php scripti joka ajastetaan suoriutumaan joka 2. sekuntti.
+    //päivittää tietokantaan jokaisen liikenteessä olevan ajoneuvon tiedot! 
+    //crontabilla ajastus
     class Receiver{
     private $data;
     private $fields=array();
-    private $dataStruct=array();
+    private $dataStruct;
     
     private $connection;
     private $db;
+    private $collection;
     
         public function __construct(){
+            
+        //variables for datagain
+           $this->dataStruct=array();
            $this->fields=array();
            $this->columns=array();
-           $this->connection = new MongoClient();
-           $this->db = $connection->hslDatabase;
-           $this->collection = $db->information;
+           
+           //Mondodb init
+           // connect
+            $this->connection = new MongoClient();
+
+            // select a database
+            $this->db = $this->connection->Data;
+
         }
         
         public function __destruct(){
             $this->connection->close();
         }
         
-        public function getData(){
-            $this->data= file_get_contents("http://83.145.232.209:10001/?type=vehicles&lng1=23&lat1=60&lng2=26&lat2=61&online=1");
+        public function getData(){ //Hakee ajoneuvojen sijainnin
             
-            $array=explode("\r\n",$this->data);
+            $this->collection = $this->db->vehicles; //soittaa vehicles kokoelmaan
+            
+            $this->data= file_get_contents("http://83.145.232.209:10001/?type=vehicles&lng1=23&lat1=60&lng2=26&lat2=61&online=1"); //Hakee reittiopas apista liikenteessä olevat linjurit
+            
+            $array=explode("\r\n",$this->data); //Räjäyttää datan taulukoiksi (rivi)
             
             foreach ($array as $rivi){
                 $tmparray=array();
                 
-                $fields=explode(";",$rivi);
+                $fields=explode(";",$rivi); //jakaa jokaisen ; merkillä erotetun osion omaksi osiokseen
                 
-                foreach ($fields as $kentta) {
+                foreach ($fields as $kentta) { //työntää tmparrayhin jokaisen taulukon jäsenen 1;2;3 jne
                     array_push($tmparray, $kentta);
                 }
                 
-                if ($tmparray[0]!=""){
-                // RHKL00098;1010;24.943583;60.165314;347;2;0;1291404;1023
-                //  ID, route, lat, lng, bearing, direction, prevstop,currentstop, departure
-                $tmparray=array("ID" => $tmparray[0],
+                
+                if ($tmparray[0]){ //jos ei ole tyhjä eikä null niin voidaan luoda assosiatiivinen taulukko 
+                
+                    // RHKL00098;1010;24.943583;60.165314;347;2;0;1291404;1023
+                    //  ID, route, lat, lng, bearing, direction, prevstop,currentstop, departure
+                    $tmparray=array("ID" => $tmparray[0],
                                 "Route"=> $tmparray[1],
                                 "lat" => $tmparray[2],
                                 "lng"=> $tmparray[3],
@@ -47,14 +65,13 @@
                                 "departure" => $tmparray[8]
                                 );
                                 
-                array_push($this->dataStruct,$tmparray);
+                        //updaten parametrit query, update, upsert
+                        $this->collection->update(array("ID" => $tmparray['ID']), $tmparray,array("upsert"=> TRUE)); //Jos on olemassa jo niin päivitetään, muuten luodaan uusi dokumentti
+                    
                 }
                 
             }
             
-            echo json_encode($this->dataStruct);
-
-           
         }
         
     }
@@ -63,7 +80,6 @@
     $vastaanotin=new Receiver();
     $vastaanotin->getData();
     
-   
     
     
 ?>
---
+++ php/updater.sh
@@ -0,0 +1,1 @@
+watch -n 4 php getloc.php
